ff95d5f61b020a63c095c173274af4d25266d225
KDU15 <carloskc127@gmail.com>
2022-12-22 04:26:40 -0300

    seg

---
+++ telas/home.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { TouchableOpacity, View, FlatList, Text, Image } from 'react-native'
+import { StackNavigationProp } from '@react-navigation/stack'
+import md5 from 'js-md5'
+import Heroi from './heroi'
+
+const PUBLIC_KEY = '6a9f7ad2bd8d09e2026a507fe68730ae'
+const PRIVATE_KEY = '0ef7be6353203aba74a7aad9eba59806791fc0b2'
+
+export default class Home extends React.PureComponent {
+    static navigationOptions = {
+        title: 'Heroes'
+    }
+    
+    state = {
+        data: []
+    }
+    
+    async componentDidMount() {
+        const timestamp = Number(new Date())
+        const hash = md5.create()
+        hash.update(timestamp + PRIVATE_KEY + PUBLIC_KEY)
+
+        const response = await fetch(`https://gateway.marvel.com/v1/public/characters?ts=${timestamp}&orderBy=name&limit=10&apikey=${PUBLIC_KEY}&hash=${hash.hex()}`)
+        const responseJson = await response.json()
+        this.setState({data: responseJson.data.results})
+    }
+
+    _renderItem = ({item}) => {
+        return  (
+            <TouchableOpacity onPress={()=>this._onItemPress(item)} style={{flexDirection:'row', padding: 10, alignItems:'center'}}>
+                <Image style={{height: 50, width: 50, borderRadius: 25}} source={{uri: `${item.thumbnail.path}.${item.thumbnail.extension}` }} />
+                <Text style={{marginLeft: 10}}>{item.name}</Text>
+            </TouchableOpacity>
+        )
+    }
+
+    _onItemPress = (item) => {
+        this.props.navigation.navigate('Heroi', {hero: item})
+    } 
+
+    render() {
+        return (
+            <FlatList 
+                data={this.state.data}
+                renderItem={this._renderItem}
+                keyExtractor={(item) => item.id}
+                ItemSeparatorComponent={()=>
+                    <View style={{height:1, backgroundColor: '#f7f7f7'}} 
+                />}
+            />
+        )
+    }
+}

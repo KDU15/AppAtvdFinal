070dde16ced2f435b8ac3edb0cc0f69202b5eafd
unknown <carloskc127@gmail.com>
2022-12-22 21:19:00 -0300

    pronto

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Home from './telas/home';
+import Herois from './telas/herois';
 import Heroi from './telas/heroi';
+import Home from './telas/home';
 
 const Stack = createStackNavigator();
 
 function App() {
   return (
-     <NavigationContainer>
+    <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Marvel API" component={Home} />
+        <Stack.Screen name="Herois" component={Herois} />
         <Stack.Screen name="Heroi" component={Heroi} />
       </Stack.Navigator>
     </NavigationContainer>
--- telas/home.js
+++ telas/home.js
@@ -1,54 +1,30 @@
-import React from 'react'
-import { TouchableOpacity, View, FlatList, Text, Image } from 'react-native'
-import { StackNavigationProp } from '@react-navigation/stack'
-import md5 from 'js-md5'
-import Heroi from './heroi'
+import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react'
+import { ImageBackground, Button, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Herois from './herois';
+import Heroi from './heroi';
 
-const PUBLIC_KEY = '6a9f7ad2bd8d09e2026a507fe68730ae'
-const PRIVATE_KEY = '0ef7be6353203aba74a7aad9eba59806791fc0b2'
+const Stack = createStackNavigator();
 
-export default class Home extends React.PureComponent {
-    static navigationOptions = {
-        title: 'Heroes'
-    }
-    
-    state = {
-        data: []
-    }
-    
-    async componentDidMount() {
-        const timestamp = Number(new Date())
-        const hash = md5.create()
-        hash.update(timestamp + PRIVATE_KEY + PUBLIC_KEY)
+const image = { uri: "https://reactjs.org/logo-og.png" };
 
-        const response = await fetch(`https://gateway.marvel.com/v1/public/characters?ts=${timestamp}&orderBy=name&limit=10&apikey=${PUBLIC_KEY}&hash=${hash.hex()}`)
-        const responseJson = await response.json()
-        this.setState({data: responseJson.data.results})
-    }
-
-    _renderItem = ({item}) => {
-        return  (
-            <TouchableOpacity onPress={()=>this._onItemPress(item)} style={{flexDirection:'row', padding: 10, alignItems:'center'}}>
-                <Image style={{height: 50, width: 50, borderRadius: 25}} source={{uri: `${item.thumbnail.path}.${item.thumbnail.extension}` }} />
-                <Text style={{marginLeft: 10}}>{item.name}</Text>
-            </TouchableOpacity>
-        )
-    }
-
-    _onItemPress = (item) => {
-        this.props.navigation.navigate('Heroi', {hero: item})
-    } 
-
-    render() {
-        return (
-            <FlatList 
-                data={this.state.data}
-                renderItem={this._renderItem}
-                keyExtractor={(item) => item.id}
-                ItemSeparatorComponent={()=>
-                    <View style={{height:1, backgroundColor: '#f7f7f7'}} 
-                />}
-            />
-        )
-    }
+function Home({navigation}){
+    return(
+        <View>
+            <ImageBackground source={image} resizeMode="cover" style={styles.image}></ImageBackground>
+            <Text>Tome</Text>
+            <Button title='Herois' onPress={() => navigation.navigate('Herois')} />
+        </View>
+    )
 }
+
+const styles = StyleSheet.create({
+    image: {
+      flex: 1,
+      justifyContent: "center"
+    }
+  });
+
+export default Home;
